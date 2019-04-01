80a16cdd1c98a6258153810c43f0d15e5941c02b
AReach <DerAReich@gmail.com>
2019-04-01 16:56:09 -0500

    completed chartOrganization function

--- drills.js
+++ drills.js
@@ -133,7 +133,7 @@ function getPossibleMoves(maze, pos) {
   }
   return moves;
 }
-                                //y , x
+//y , x
 function solveMaze(maze, pos = [0, 0]) {
   if ( maze[pos[0]][pos[1]] === 'e' ) {
     return 'e';
@@ -192,12 +192,87 @@ function solveAllMaze(maze, pos = [0, 0], prevMoves = '') {
   }
 }
 
-let maze = [
-  [' ', ' ', ' ', '*', ' ', ' ', ' '],
-  ['*', '*', ' ', '*', ' ', '*', ' '],
-  [' ', ' ', ' ', ' ', ' ', ' ', ' '],
-  [' ', '*', '*', '*', '*', '*', ' '],
-  [' ', ' ', ' ', ' ', ' ', ' ', 'e']
-];
+// let maze = [
+//   [' ', ' ', ' ', '*', ' ', ' ', ' '],
+//   ['*', '*', ' ', '*', ' ', '*', ' '],
+//   [' ', ' ', ' ', ' ', ' ', ' ', ' '],
+//   [' ', '*', '*', '*', '*', '*', ' '],
+//   [' ', ' ', ' ', ' ', ' ', ' ', 'e']
+// ];
 
-solveAllMaze(maze);
+let organization = {
+  'Zuckerberg': {        
+    'Schroepfer': {
+      'Bosworth': {
+        'Steve':{},
+        'Kyle':{},
+        'Andra':{}
+      },
+      'Zhao': {
+        'Richie':{},
+        'Sofia':{},
+        'Jen':{}
+      },
+      'Badros': {
+        'John':{},
+        'Mike':{},
+        'Pat':{}
+      },
+      'Parikh': {
+        'Zach':{},
+        'Ryan':{},
+        'Tes':{}
+      }
+    },
+    'Schrage': {
+      'VanDyck': {
+        'Sabrina':{},
+        'Michelle':{},
+        'Josh':{}
+      },
+      'Swain': {
+        'Blanch':{},
+        'Tom':{},
+        'Joe':{}
+      },
+      'Frankovsky': {
+        'Jasee':{},
+        'Brian':{},
+        'Margaret':{}
+      }
+    },
+    'Sandberg': {
+      'Goler': {
+        'Eddie':{},
+        'Julie':{},
+        'Annie':{}
+      },
+      'Hernandez': {
+        'Rowi':{},
+        'Inga':{},
+        'Morgan':{}
+      },
+      'Moissinac': {
+        'Amy':{},
+        'Chuck':{},
+        'Vinni':{}
+      },
+      'Kelley': {
+        'Eric':{},
+        'Ana':{},
+        'Wes':{}
+      }
+    }}};
+
+function chartOrganization(org, step = 0) {
+  const keys = Object.keys(org);
+  const tab = '   ';
+  if (keys.length > 0) {
+    keys.forEach(key => {
+      console.log(tab.repeat(step) + key);
+      chartOrganization(org[key], step+1);
+    });
+  }
+}
+
+// chartOrganization(organization);
