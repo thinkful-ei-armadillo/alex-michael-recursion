179484b0d3eaabc9106df6fed0c44f49af4d56dc
Michael Bramble <mike.w.bramble@gmail.com>
2019-04-01 14:14:23 -0400

    7

--- drills.js
+++ drills.js
@@ -77,4 +77,11 @@ function fib(n, sum = 1, prev = 0) {
   return `, ${sum}` + fib(n-1, newSum, newPrev);
 }
 
-console.log(fib(7));
+function factorial(num){
+  if(num === 0){
+    return 1;
+  }
+  return num * factorial(num - 1);
+}
+
+console.log(factorial(5));
